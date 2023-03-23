fe0d36e45c27486e0156b86b019da281c5de8897
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-03-23 07:38:59 +0000

    render recipe items from recipe object

--- src/routes/CookingItem.svelte
+++ src/routes/CookingItem.svelte
@@ -1,30 +1,45 @@
 <script>
-  let cooked = false;
+	export let name;
+	export let cooked = false;
+	export let prep;
+	export let cook;
+
+	const toggleCooked = () => (cooked = !cooked);
 </script>
 
-<div class="row">
+<div class="row" class:cooked on:click={toggleCooked}>
 	<div class="details">
-		<h5>Paleo Breakfast Bowl</h5>
-    <div class="details">Prep time: 15 minutes</div>
-    <div class="details">Cook time: 30 minutes</div>
+		<h5>{name}</h5>
+		{#if !cooked}
+			{#if prep}<div>Prep time: {prep}</div>{/if}
+			{#if cook}<div>Cook time: {cook}</div>{/if}
+		{/if}
 	</div>
-  <button class="button is-black is-outline">Cook!</button>
+	<button class="button is-black">{cooked ? 'Done ✓' : 'Cook!'}</button>
 </div>
 
 <style>
 	.row {
 		display: flex;
-    align-items: start;
-    justify-content: space-between;
+		align-items: start;
+		justify-content: space-between;
 		padding: 2rem 0;
 		border-bottom: 2px solid black;
 	}
 
-  .row:first-of-type {
-    padding-top: 0;
-  }
+	.row:first-of-type {
+		padding-top: 0;
+	}
 
 	h5 {
 		margin: 0;
+	}
+
+	button {
+		width: 8rem;
+	}
+
+	.cooked button {
+		padding: 0.4rem 1rem;
 	}
 </style>
--- src/routes/CookingList.svelte
+++ src/routes/CookingList.svelte
@@ -1,11 +1,88 @@
 <script>
 	import CookingItem from './CookingItem.svelte';
+
+	const recipes = [
+		{
+			name: 'Paleo Breakfast Bowl',
+			cooked: true,
+			tags: ['breakfast', 'paleo'],
+			prep: '15 minutes',
+			cook: '30 minutes',
+			ingredients: [
+				'2 sweet potatoes, peeled and cubed',
+				'1 red bell pepper, chopped',
+				'1 yellow onion, chopped',
+				'1 tbsp olive oil',
+				'Salt and pepper',
+				'4 large eggs',
+				'1 avocado, sliced'
+			],
+			method: [
+				'Preheat the oven to 400°F (200°C).',
+				'Toss the sweet potatoes, bell pepper, and onion in olive oil and sprinkle with salt and pepper.',
+				'Spread the veggies out on a baking sheet and bake for 20-25 minutes, or until the sweet potatoes are soft.',
+				'Crack the eggs into a non-stick pan and cook to your desired doneness.',
+				'Divide the sweet potato mixture into four bowls and top each with an egg and sliced avocado.'
+			]
+		},
+		{
+			name: 'Paleo Chicken Salad',
+			cooked: false,
+			tags: ['lunch', 'dinner', 'paleo', '30min'],
+			prep: '20 minutes',
+			cook: undefined,
+			ingredients: [
+				'2 chicken breasts, cooked and shredded',
+				'2 stalks celery, chopped',
+				'1/4 cup red onion, chopped',
+				'1/4 cup paleo mayo',
+				'1 tbsp dijon mustard',
+				'1 tbsp fresh lemon juice',
+				'Salt and pepper',
+				'Lettuce leaves'
+			],
+			method: [
+				'In a large bowl, combine the chicken, celery, and red onion.',
+				'In a small bowl, whisk together the mayo, dijon mustard, lemon juice, and a pinch of salt and pepper.',
+				'Pour the dressing over the chicken mixture and stir to combine.',
+				'Serve on lettuce leaves.'
+			]
+		},
+		{
+			name: 'Paleo Beef Stir Fry',
+			cooked: false,
+			tags: ['lunch', 'dinner', 'paleo', '30min'],
+			prep: '10 minutes',
+			cook: '20 minutes',
+			ingredients: [
+				'1 lb flank steak, thinly sliced',
+				'2 tbsp coconut oil',
+				'1 red bell pepper, sliced',
+				'1 yellow onion, sliced',
+				'2 cloves garlic, minced',
+				'1 tbsp fresh ginger, minced',
+				'2 tbsp coconut aminos',
+				'Salt and pepper',
+				'Green onions, sliced (for garnish)'
+			],
+			method: [
+				'Heat the coconut oil in a large pan over high heat.',
+				'Add the beef and cook until browned, about 3-4 minutes.',
+				'Remove the beef from the pan and set aside.',
+				'Add the bell pepper and onion to the same pan and cook until softened, about 5 minutes.',
+				'Add the garlic and ginger and cook for an additional minute.',
+				'Add the beef back to the pan, along with the coconut aminos and a pinch of salt and pepper. Cook for 1-2 more minutes, until everything is heated through.',
+				'Serve hot, garnished with sliced green onions.'
+			]
+		}
+	];
 </script>
 
 <div>
 	<h3>Let's cook!</h3>
-	<CookingItem />
-	<CookingItem />
+	{#each recipes as recipe}
+		<CookingItem {...recipe} />
+	{/each}
 </div>
 
 <style>
