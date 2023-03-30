034c9aa9dc0843f3e854aa0bf1ed53343a0f4d21
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-03-30 05:05:44 +0100

    add plan page

---
+++ src/routes/plan/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { MEAL_PLANS } from './plans.js';
+</script>
+
+<svelte:head>
+	<title>Plan</title>
+</svelte:head>
+
+<div class="card">
+	<h5>Current plan</h5>
+	<x-cell><h4 class="plan">Paleo week 4 (day 3 of 7) ✏️</h4></x-cell>
+</div>
+<div class="card">
+	<h5>Next plan</h5>
+</div>
+<h1>Meal plans</h1>
+<ul class="plans-list">
+	{#each MEAL_PLANS as plan}
+		<li>{plan.name}</li>
+	{/each}
+</ul>
+
+<style>
+	.card {
+		padding: 1rem;
+		margin-bottom: 1rem;
+		background: black;
+		color: white;
+	}
+	.card h4,
+	h5 {
+		color: white;
+	}
+
+	.plans-list li {
+		list-style-type: none;
+		margin: 0;
+	}
+</style>
