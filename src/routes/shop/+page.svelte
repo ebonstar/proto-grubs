71603766693bfc805e1d02b49e227da1f8079d35
ebonstar <18689574+ebonstar@users.noreply.github.com>
2023-05-18 11:27:05 +0100

    add shopping page

---
+++ src/routes/shop/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Shopping List</title>
+</svelte:head>
+
+<div>
+	<h2>Shopping List</h2>
+	<div class="ingredients">
+		<h4>Ingredients</h4>
+		<h6>Spiced carrot & lentil soup:</h6>
+		<ul class="no-style">
+			<li><input type="checkbox" />2 tsp cumin seeds</li>
+			<li><input type="checkbox" />pinch chilli flakes</li>
+			<li><input type="checkbox" />2 tbsp olive oil</li>
+			<li><input type="checkbox" />600g carrots</li>
+			<li><input type="checkbox" />140g split red lentils</li>
+			<li><input type="checkbox" />1l hot vegetable stock</li>
+			<li><input type="checkbox" />125ml milk</li>
+			<li><input type="checkbox" />plain yogurt and naan bread</li>
+		</ul>
+	</div>
+</div>
+
+<style>
+	input {
+		margin-right: 1rem;
+	}
+</style>
